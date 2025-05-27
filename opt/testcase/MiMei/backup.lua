b7267e72402e486589929d5cd825fd948e0e4722
3and4 <lishibao@gmail.com>
2025-05-27 17:35:21 +0800

    递交rpc

--- opt/testcase/MiMei/backup.lua
+++ opt/testcase/MiMei/backup.lua
@@ -27,24 +27,33 @@ if (err ~= nil) then
 	return err
 end
 
+--测试的基准数量
+local TestCount = 100 
 
-
-
-
---写100个值
-for i=0, 100, 1 do
+--生成一个随机值
+local idxDel = os.time() % TestCount 
+local count
+--写基准数量个值
+for i=0, TestCount, 1 do
 	local key = string.format("key%d", i)
-	local value =string.format("value%d", i)
-	print("set", key, value)
-	err = mm.Set(mmsid, key, value);
+	if (i == idxDel) then
+		count, err = mm.Del(mmsid, key)
+		print("del", key)
+	else 
+		local value =string.format("value%d", i)
+		print("set", key, value)
+		err = mm.Set(mmsid, key, value);
+	end
 	if (err ~= nil) then
-		print('Set err=%v',  err);
+		print('err=%v',  err);
 		return err
 	end
 end
 
+
+
 --写一下hashtable
-for i=0, 100, 1 do
+for i=0, TestCount, 1 do
 	local field = string.format("field%d", i)
 	local value =string.format("value%d", i)
 	print("hset", "hkey", field, value)
@@ -55,9 +64,16 @@ for i=0, 100, 1 do
 	end
 end
 
+--制造一些变动差异
+err = mm.Set(mmsid, 'sidend', sid);
+if (err ~= nil) then
+	print('Set err',  err);
+	return err
+end
+
 
 --显示值
-for i=0, 100, 1 do
+for i=0, TestCount, 1 do
 	local key = string.format("key%d", i)
 	value, err = mm.Get(mmsid, key);
 	if (err ~= nil) then
@@ -68,7 +84,7 @@ for i=0, 100, 1 do
 	print("Get", key, value);
 end
 
-for i=0, 100, 1 do
+for i=0, TestCount, 1 do
 	local field = string.format("field%d", i)
 	value, err = mm.HGet(mmsid, "hkey", field);
 	if (err ~= nil) then
@@ -79,17 +95,35 @@ for i=0, 100, 1 do
 	print("HGet", "hkey", field, value);
 end
 
+local sp = scorepair.new(1, 'm1')
+print("sp", sp:getScore(), sp:getMember());
+
+local n, err =mm.ZAdd(mmsid, 'zkey', sp)
+if (err ~= nil) then
+	print('ZAdd err',  err);
+	return err
+end
+print("ZAdd n=", n)
+--[[
+local ret, err = mm.Zrem(mmsid, "zkey", "m1")
+if (err ~= nil) then
+        print("Zrem err", err);
+        return err
+end
+print("Zrem ret=", ret)
+--]]
+
 --备份
 --	MMBackup(sid, mid, memo string)error
-err = mm.MMBackup(sid, mid, '')
+local ver
+ver, err = mm.MMBackup(sid, mid, '')
 if (err ~= nil) then
-	print('MMBackup err=%v',  err);
+	print('MMBackup err=',  err);
 	return err
 end
 
 
---异步接收备份消息
-local ver
+--[[异步接收备份消息
 local msg = require('msg');
 while true
 do
@@ -104,20 +138,20 @@ do
 	reg = "(%a+)=(.*)"
 	_, _, key, value = string.find(info, reg)
 	
-	if (key=="ver") then
+	if (key=="result") then
 		print("ver", value)
 		ver = value
 		--return value
 		break
 	end
-	if (key=="err") then
+	if (key=="error") then
 		print("err", value)
 		return value
 	end	
 	
 	print(key, value)
 end
-
+--]]
 print("ver", ver)
 --[[
 
@@ -164,10 +198,17 @@ end
 
 local value, err = mm.Get(mmsid, "sid")
 if (err ~= nil) then
-	print('HMGet err=',  err);
+	print('Get sid err=',  err);
 	return err
 end
 print("read sid", value)
+
+local value, err = mm.Get(mmsid, "sidend")
+if (err ~= nil) then
+	print('Get sidend err=',  err);
+	return err
+end
+print("read sidend", value)
 
 --[[
 fvs, err = mm.HMGet(mmsid, "hkey", "field0", "field1")
