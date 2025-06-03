0deec0554c66f533c5338f1b1629f4ee5e3e91aa
3and4 <lishibao@gmail.com>
2025-06-03 17:25:31 +0800

    加入VarActStub LApiStub 加入第一个示例

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,3 +1,13 @@
 package rpc
 
 // Package rpc provides RPC functionality for the Leither project
+type LApiStub struct {
+	VarActStub
+}
+
+type VarActStub struct {
+	GetVar          func(sid, name string, args ...string) (any, error)
+	GetVarByContext func(sid, name string, mapOpt map[string]string) (any, error)
+	Act             func(sid, name string, args ...string) error
+	GetGobVar       func(sid, name string, args ...string) ([]byte, error)
+}
