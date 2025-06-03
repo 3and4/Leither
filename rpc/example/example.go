61ab6d75718bc61673bbb5bd3c64f29904da6aa2
3and4 <lishibao@gmail.com>
2025-06-03 18:28:00 +0800

    加入InitLApiStubByUrl

--- rpc/example/example.go
+++ rpc/example/example.go
@@ -4,28 +4,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/3and4/Leither/rpc"
-	"github.com/hprose/hprose-golang/v3/rpc/core"
-	. "github.com/hprose/hprose-golang/v3/rpc/websocket"
+	api "github.com/3and4/Leither/rpc"
 )
 
-func init() {
-	RegisterHandler()
-	RegisterTransport()
-}
-
 func main() {
-	var stub rpc.LApiStub
-	// var stub struct {
-	// 	GetVar func(sid, name string, args ...string) (any, error)
-	// }
 
-	client := core.NewClient("ws://127.0.0.1:4800/ws3/")
-	// client.Use(log.Plugin)
-	// var proxy struct {
-	// 	Hello func(name string) (string, error)
-	// }
-	client.UseService(&stub)
+	stub := api.InitLApiStubByUrl("127.0.0.1:4800")
 	ver, err := stub.GetVar("", "ver")
 	if err != nil {
 		panic(err)
